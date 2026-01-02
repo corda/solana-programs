28a0473e99cbcb8971c047b3422d827959667e35
Shams Asari <shams.asari@r3.com>
2026-01-02 10:56:47 +0000

    Removed savaGithubPackages

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,6 @@ pluginManagement {
     mavenLocal()
     gradlePluginPortal()
     maven {
-      name = "savaGithubPackages"
-      url = uri("https://maven.pkg.github.com/sava-software/sava-build")
-      credentials(PasswordCredentials::class)
-    }
-    maven {
       url = uri("https://software.r3.com/artifactory/corda-dependencies")
       credentials {
         username = System.getenv("CORDA_ARTIFACTORY_USERNAME")
